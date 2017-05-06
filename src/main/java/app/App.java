c1e0b3d56a25eb69be91cf1f931b4e5bf63a609e
Sean Francis N. Ballais <sean@seanballais.com>
2017-05-06 23:27:54 +0800

    Added GUI.

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -3,6 +3,7 @@ package app;
 import app.modules.Compressor;
 import app.modules.Decompressor;
 import app.modules.Trainer;
+import app.utils.ds.HuffmanDistribution;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.uiDesigner.core.Spacer;
@@ -12,6 +13,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -44,6 +46,7 @@ public class App {
 
         openImageButton.addActionListener(e -> {
             JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
             fileChooser.setFileFilter(new FileNameExtensionFilter(".PNG files", "png", "PNG"));
             fileChooser.setDialogTitle("Open a PNG image");
             fileChooser.showOpenDialog(null);
@@ -56,16 +59,18 @@ public class App {
             }
         });
         trainNewHuffmanTreeButton.addActionListener(e -> {
+            huffmanTrainer = new Trainer();
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            fileChooser.setDialogTitle("Choose directory to store the trained huffman file.");
+            fileChooser.setDialogTitle("Choose directory to store the trained huffman file");
             fileChooser.showOpenDialog(null);
             File currentDir = fileChooser.getSelectedFile();
+            long startTime = System.currentTimeMillis();
             if (currentDir != null) {
                 statusLabel.setText("Training data from pre-selected image.");
                 try {
                     huffmanTrainer.trainFromImage(currentFile.getAbsolutePath());
-                    huffmanTrainer.saveToFile(currentDir.getAbsolutePath());
+                    huffmanTrainer.saveToFile(currentDir.getAbsolutePath() + "/");
                 } catch (IOException ex) {
                     JOptionPane.showMessageDialog(
                             null,
@@ -77,7 +82,142 @@ public class App {
                     return;
                 }
 
-                statusLabel.setText("Data trained successfully.");
+                statusLabel.setText("Data trained successfully in " + ((System.currentTimeMillis() - startTime) / 1000) + " seconds.");
+                compressImageButton.setEnabled(true);
+                openHuffmanCodedImageButton.setEnabled(true);
+            }
+        });
+        trainExistingHuffmanTreeButton.addActionListener(e -> {
+            huffmanTrainer = new Trainer();
+            JFileChooser fileChooser = new JFileChooser();
+
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            fileChooser.setDialogTitle("Choose existing Huffman file");
+            fileChooser.setFileFilter(null);
+            fileChooser.showOpenDialog(null);
+            File currentHuffFile = fileChooser.getSelectedFile();
+            long startTime = System.currentTimeMillis();
+            if (currentHuffFile != null) {
+                try {
+                    statusLabel.setText("Getting distribution from " + currentHuffFile.getAbsolutePath() + ".");
+                    huffmanTrainer.getDistributionFromFile(currentHuffFile.getAbsolutePath());
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            ex.getMessage(),
+                            "An error occurred while reading selected file",
+                            JOptionPane.ERROR_MESSAGE
+                    );
+                }
+            }
+
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            fileChooser.setDialogTitle("Choose directory to store the trained huffman file");
+            long midTime = System.currentTimeMillis();
+            fileChooser.showOpenDialog(null);
+            File currentDir = fileChooser.getSelectedFile();
+            midTime = System.currentTimeMillis() - midTime;
+            startTime -= midTime;
+            if (currentDir != null) {
+                statusLabel.setText("Training data from pre-selected image.");
+                try {
+                    huffmanTrainer.trainFromImage(currentFile.getAbsolutePath());
+                    huffmanTrainer.saveToFile(currentDir.getAbsolutePath() + "/");
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(
+                            null,
+                            ex.getMessage(),
+                            "An error occurred while training data",
+                            JOptionPane.ERROR_MESSAGE
+                    );
+
+                    return;
+                }
+
+                statusLabel.setText("Data trained successfully in " + ((System.currentTimeMillis() - startTime) / 1000) + " seconds.");
+                compressImageButton.setEnabled(true);
+                openHuffmanCodedImageButton.setEnabled(true);
+            }
+        });
+        compressImageButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+
+            try {
+                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                fileChooser.setDialogTitle("Choose existing Huffman file");
+                fileChooser.setFileFilter(new FileNameExtensionFilter(".HUFF files", "huff", "HUFF"));
+                fileChooser.showOpenDialog(null);
+                File currentHuffFile = fileChooser.getSelectedFile();
+                long startTime = System.currentTimeMillis();
+                if (currentHuffFile != null) {
+                    statusLabel.setText("Getting distribution from " + currentHuffFile.getAbsolutePath() + ".");
+                    huffmanCompressor.generateDistributionFromFile(currentHuffFile.getAbsolutePath());
+
+                    long midTime = System.currentTimeMillis();
+                    fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                    fileChooser.setFileFilter(null);
+                    fileChooser.setDialogTitle("Choose directory to save the compressed image");
+                    fileChooser.showOpenDialog(null);
+                    File currentDir = fileChooser.getSelectedFile();
+                    midTime = System.currentTimeMillis() - midTime;
+                    startTime -= midTime;
+                    if (currentDir != null) {
+                        statusLabel.setText("Saving compressed image in " + currentDir.getAbsolutePath() + ".");
+                        huffmanCompressor.compressImage(
+                                currentFile.getAbsolutePath(), currentDir.getAbsolutePath(), "output-img"
+                        );
+                        statusLabel.setText("Compression duration: " + ((System.currentTimeMillis() - startTime) / 1000) + " seconds. Compressed file saved in " + currentDir.getAbsolutePath() + ".");
+                    }
+                }
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(
+                        null,
+                        ex.getMessage(),
+                        "An error occurred while reading selected file",
+                        JOptionPane.ERROR_MESSAGE
+                );
+            }
+        });
+        openHuffmanCodedImageButton.addActionListener(e -> {
+            JFileChooser fileChooser = new JFileChooser();
+
+            try {
+                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                fileChooser.setDialogTitle("Choose existing Huffman file");
+                fileChooser.setFileFilter(new FileNameExtensionFilter(".HUFF files", "huff", "HUFF"));
+                fileChooser.showOpenDialog(null);
+                File currentHuffFile = fileChooser.getSelectedFile();
+                long startTime = System.currentTimeMillis();
+                if (currentHuffFile != null) {
+                    statusLabel.setText("Getting distribution from " + currentHuffFile.getAbsolutePath() + ".");
+                    huffmanCompressor.generateDistributionFromFile(currentHuffFile.getAbsolutePath());
+
+                    long midTime = System.currentTimeMillis();
+                    fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                    fileChooser.setFileFilter(new FileNameExtensionFilter(".PNB files", "pnb", "PNB"));
+                    fileChooser.setDialogTitle("Choose image to decompress (.pnb)");
+                    fileChooser.showOpenDialog(null);
+                    File compressedFile = fileChooser.getSelectedFile();
+                    midTime = System.currentTimeMillis() - midTime;
+                    startTime -= midTime;
+                    if (compressedFile != null) {
+                        statusLabel.setText("Decompressing image " + compressedFile.getAbsolutePath() + ".");
+                        HuffmanDistribution distribution = new HuffmanDistribution();
+                        distribution.generateDistributionFromFile(currentHuffFile.getAbsolutePath());
+                        huffmanDecompressor.generateTree(distribution);
+                        BufferedImage decompressedImage = huffmanDecompressor.decompress(compressedFile.getAbsolutePath());
+                        compressedImage.setIcon(new ImageIcon(decompressedImage));
+
+                        statusLabel.setText("Decompression duration: " + ((System.currentTimeMillis() - startTime) / 1000) + " seconds. Finished decompressing file " + compressedFile.getAbsolutePath() + ".");
+                    }
+                }
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(
+                        null,
+                        ex.getMessage(),
+                        "An error occurred while reading selected file",
+                        JOptionPane.ERROR_MESSAGE
+                );
             }
         });
     }
@@ -133,7 +273,7 @@ public class App {
         buttonPanel.setLayout(new GridLayoutManager(1, 5, new Insets(0, 0, 0, 0), -1, -1));
         guiPanel.add(buttonPanel, BorderLayout.NORTH);
         openHuffmanCodedImageButton = new JButton();
-        openHuffmanCodedImageButton.setEnabled(false);
+        openHuffmanCodedImageButton.setEnabled(true);
         openHuffmanCodedImageButton.setText("Open Huffman Coded Image");
         buttonPanel.add(openHuffmanCodedImageButton, new GridConstraints(0, 4, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         compressImageButton = new JButton();
@@ -156,7 +296,7 @@ public class App {
         guiPanel.add(statusPanel, BorderLayout.SOUTH);
         statusPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLoweredBevelBorder(), null));
         statusLabel = new JLabel();
-        statusLabel.setText("    ");
+        statusLabel.setText("Nothing particularly interesting right now, unless realizing that I am merely a series of bits is fun.");
         statusPanel.add(statusLabel, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, new Dimension(-1, 24), new Dimension(-1, 24), new Dimension(-1, 24), 0, false));
     }
 
